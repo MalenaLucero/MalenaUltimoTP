9ebd2de17c22509b3a4b688aefc4355622e701ed
MalenaLucero <luceromalena@hotmail.com>
2019-11-20 22:25:24 -0300

    routes from AppRouter

--- ultimo-tp/src/AppRouter.js
+++ ultimo-tp/src/AppRouter.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './Home'
+import Results from './Results'
+import Payment from './Payment'
 
 const AppRouter = () =>{
     return(
@@ -8,6 +10,8 @@ const AppRouter = () =>{
             <Route exact path={'/'}>
                 <Home/>
             </Route>
+            <Route exact path={'/results'} component={Results}/>
+            <Route exact path={'/payment'} component={Payment}/>
         </Router>
     )
 }
