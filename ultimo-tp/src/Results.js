17729361444fbaa2946cd9e1d0133fa7ca04fba1
MalenaLucero <luceromalena@hotmail.com>
2019-11-23 21:33:20 -0300

    filter by adeparture and arrival

--- ultimo-tp/src/Results.js
+++ ultimo-tp/src/Results.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import FetchData from './components/FetchData'
+
+
 
 const Results = () =>{
+    //const [ flightSearch, setFlightSearch ] = useState(FlightModel)
+    const [ flights, setFlights ] = useState([ '' ])
+
+    const FlightModel = {
+        departure: 'SYD', 
+        arrival: 'BKK', 
+        departureDate: '', 
+        arrivalDate: '', 
+        passengers: ''
+    }
+
+    useEffect(() => {
+		async function getTrip() {
+			const response = await FetchData()
+            let foundFlights = response.data.filter(f=>f.itineraries[0].segments[0].departure.iataCode === 'SYD')
+                .filter(f=>f.itineraries[0].segments[f.itineraries[0].segments.length -1 ].arrival.iataCode === 'BKK')
+            console.log(foundFlights)
+			//setFlights(response.data)
+			//toogleLoading(false)
+		}
+		getTrip()
+	}, [])
     return(
-        <div>Results</div>
+        <div>{FlightModel.departure}</div>
+        
     )
 }
 
