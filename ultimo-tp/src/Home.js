0518062969f98d1c7425d722654fe41da86e15f1
MalenaLucero <luceromalena@hotmail.com>
2019-11-29 12:16:05 -0300

    trying out formik

--- ultimo-tp/src/Home.js
+++ ultimo-tp/src/Home.js
@@ -5,9 +5,17 @@ import SellingPoints from './components/SellingPoints'
 import FlightModel from './helpers/FlightModel'
 import ContactNav from './components/ContactNav'
 
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import { useFormik } from 'formik';
+//import * as Yup from 'yup'
+
 
 const Home = () => {
 	const [ flightSearch, setFlightSearch ] = useState(FlightModel)
+
+	const test = ''
+	const [testForm, setTestForm] = useState(test)
+	console.log(test)
 	return (
 		<div>
 			<h1>Cheap flights best deals</h1>
@@ -23,6 +31,21 @@ const Home = () => {
 				<ContactNav/>
 				<p>Copyright © <a href={'index.html'}>Malena Lucero</a> 2019. All rights reserved.</p>
 			</footer>
+			<Formik
+                initialValues={test}
+                onSubmit={setTestForm}
+                enableReinitialize
+            >
+                {(props)=>{
+                    return(
+                        <Form>
+                            <Field type={'text'} name={'firstName'} placeholder={'nombre'}/>
+                            <ErrorMessage name={'firstName'}/>
+                            <button type={'submit'}>Enviar</button>
+                        </Form>
+                    )
+                }}
+            </Formik>
 		</div>
 	)
 }
