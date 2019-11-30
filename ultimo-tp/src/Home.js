f399c5bdffff663d4e390fd56779bf034309113b
MalenaLucero <luceromalena@hotmail.com>
2019-11-29 21:50:14 -0300

    formik and link dont quite work yet

--- ultimo-tp/src/Home.js
+++ ultimo-tp/src/Home.js
@@ -5,47 +5,23 @@ import SellingPoints from './components/SellingPoints'
 import FlightModel from './helpers/FlightModel'
 import ContactNav from './components/ContactNav'
 
-import { Formik, Form, Field, ErrorMessage } from 'formik'
-import { useFormik } from 'formik';
 //import * as Yup from 'yup'
-
 
 const Home = () => {
 	const [ flightSearch, setFlightSearch ] = useState(FlightModel)
-
-	const test = ''
-	const [testForm, setTestForm] = useState(test)
-	console.log(test)
 	return (
 		<div>
 			<h1>Cheap flights best deals</h1>
 			<h4>Search hundreds of travel sites at once</h4>
-			<form>
-				<MainInput flightSearch={flightSearch} setFlightSearch={setFlightSearch}/>
-				<button>
-					<Link to={`results/${btoa(JSON.stringify(flightSearch))}`}>Confirm</Link>	
-				</button>
-			</form>
+			<MainInput flightSearch={flightSearch} setFlightSearch={setFlightSearch}/>
+			<button>
+				<Link to={`results/${btoa(JSON.stringify(flightSearch))}`}>Confirm</Link>	
+			</button>
 			<SellingPoints/>
 			<footer>
 				<ContactNav/>
 				<p>Copyright © <a href={'index.html'}>Malena Lucero</a> 2019. All rights reserved.</p>
 			</footer>
-			<Formik
-                initialValues={test}
-                onSubmit={setTestForm}
-                enableReinitialize
-            >
-                {(props)=>{
-                    return(
-                        <Form>
-                            <Field type={'text'} name={'firstName'} placeholder={'nombre'}/>
-                            <ErrorMessage name={'firstName'}/>
-                            <button type={'submit'}>Enviar</button>
-                        </Form>
-                    )
-                }}
-            </Formik>
 		</div>
 	)
 }
--- ultimo-tp/src/components/MainInput.js
+++ ultimo-tp/src/components/MainInput.js
@@ -1,20 +1,87 @@
 import React, {useState} from 'react'
+import { useFormik } from 'formik';
+import {Link} from 'react-router-dom'
+import FlightModel from '../helpers/FlightModel'
 
 const MainInput = ({flightSearch, setFlightSearch}) =>{
+    const {iataOrigin, iataDest, fromDate, toDate, adults, oneWay} = FlightModel
+    const redirectLink = <Link to={`results/${btoa(JSON.stringify(flightSearch))}`}>Confirm</Link>
+    console.log(flightSearch)
+    const formik = useFormik({
+        initialValues:{
+            iataOrigin,
+            iataDest,
+            fromDate,
+            toDate,
+            adults,
+            oneWay
+        },
+        onSubmit: values => {
+            setFlightSearch(values)
+            
+        },
+    })
     return(
+        <form onSubmit={formik.handleSubmit}>
+            <label htmlFor="iataOrigin">Origin</label>
+            <input
+                id="iataOrigin"
+                name="iataOrigin"
+                type="iataOrigin"
+                onChange={formik.handleChange}
+                value={formik.values.iataOrigin}
+            />
+            <label htmlFor="iataDest">Destination</label>
+            <input
+                id="iataDest"
+                name="iataDest"
+                type="iataDest"
+                onChange={formik.handleChange}
+                value={formik.values.iataDest}
+            />
+            <input
+                id="fromDate"
+                name="fromDate"
+                type="fromDate"
+                onChange={formik.handleChange}
+                value={formik.values.fromDate}
+            />
+            <input
+                id="toDate"
+                name="toDate"
+                type="toDate"
+                onChange={formik.handleChange}
+                value={formik.values.toDate}
+            />
+            <input
+                id="adults"
+                name="adults"
+                type="adults"
+                onChange={formik.handleChange}
+                value={formik.values.adults}
+            />
+            <button type="submit">
+                <Link to={`results/${btoa(JSON.stringify(flightSearch))}`}>Confirm</Link>
+            </button>
+        </form>
+    )
+
+
+
+    /*return(
         <div>
-            <input type="text" placeholder={'departure'} value={flightSearch.iataOrigin} onChange={(e)=>setFlightSearch({...flightSearch, iataOrigin: e.target.value.toString()})}/>
+            {<input type="text" placeholder={'departure'} value={flightSearch.iataOrigin} onChange={(e)=>setFlightSearch({...flightSearch, iataOrigin: e.target.value.toString()})}/>
             <input type="text" placeholder={'arrival'} value={flightSearch.iataDest} onChange={(e)=>setFlightSearch({...flightSearch, iataDest: e.target.value.toString()})}/>
             <input type="text" placeholder={'departure Date'} value={flightSearch.fromDate} onChange={(e)=>setFlightSearch({...flightSearch, fromDate: e.target.value.toString()})}/>
             <input type="text" placeholder={'arrival Date'} value={flightSearch.toDate} onChange={(e)=>setFlightSearch({...flightSearch, toDate: e.target.value.toString()})}/>
-            <input type="text" placeholder={'passengers'} value={flightSearch.adults} onChange={(e)=>setFlightSearch({...flightSearch, adults: e.target.value.toString()})}/>
+            <input type="text" placeholder={'passengers'} value={flightSearch.adults} onChange={(e)=>setFlightSearch({...flightSearch, adults: e.target.value.toString()})}/>}
             
             <label htmlFor={'roundTrip'}>Round trip</label>
             <input id={'roundTrip'} type="radio" value={flightSearch.oneWay} checked={flightSearch.oneWay ? false : true} onChange={()=>setFlightSearch({...flightSearch, oneWay: false})}/>
             <label htmlFor={'oneWay'}>One way</label>
             <input id={'oneWay'} type="radio" value={flightSearch.oneWay} checked={flightSearch.oneWay ? true : false} onChange={()=>setFlightSearch({...flightSearch, oneWay: true})}/>
         </div>
-    )
+    )*/
 }
 
 export default MainInput
