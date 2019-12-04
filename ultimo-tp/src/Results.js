07205d5dccf47e9e3c54022e138187d9b9a79512
MalenaLucero <luceromalena@hotmail.com>
2019-12-04 20:11:33 -0300

    ResultItems styles

--- ultimo-tp/src/Results.js
+++ ultimo-tp/src/Results.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import FetchData from './helpers/FetchData'
 import ResultItem from './components/ResultItem'
+import './Results.scss'
 
 const Results = ({match}) =>{
     const flightSearch = JSON.parse(atob(match.params.flight))
@@ -20,18 +21,23 @@ const Results = ({match}) =>{
 		getTrip()
 	}, [])
     return(
-        <div>
-            <h2>Results</h2>
-            <nav>
+        <div className={'resultsContainer'}>
+            <div className={'filters'}>Filters</div>
+            <div className={'results'}>
+               <nav>
                 <span>Sort by:</span>
                 <button >Price</button>
                 <button >Duration</button>
                 <button >Recommended</button>
                 <button >Airline</button>
-            </nav>
-            {isLoading ? <p>Loading...</p> : null}
-            {showResults && flights.length !== 0 ? flights.map((f,i)=><ResultItem key={i} flight={f} flightSearch={flightSearch}/>) : null}
-            {showResults && flights.length === 0 ? <p>No flights were found</p> : null}
+                </nav>
+                <div>
+                    {isLoading ? <p>Loading...</p> : null}
+                    <ul>{showResults && flights.length !== 0 ? flights.map((f,i)=><ResultItem key={i} flight={f} flightSearch={flightSearch}/>) : null}</ul>
+                    {showResults && flights.length === 0 ? <p>No flights were found</p> : null}  
+                </div>
+            </div>
+            <div className={'advertisements'}>Adverts</div>
         </div>
     )
 }
--- ultimo-tp/src/components/ResultItem.js
+++ ultimo-tp/src/components/ResultItem.js
@@ -1,27 +1,46 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import FlightDurationConverter from '../helpers/FlightDurationConverter'
+import './ResultItem.scss'
 
 const ItemContent = ({data}) =>{
     const {duration, segments} = data
     return(
-        <ul>
-            <li>Duration: {FlightDurationConverter(duration)}</li>
-            <li>Desde: {segments[0].departure.iataCode}</li>
-            <li>Hasta: {segments[segments.length-1].arrival.iataCode}</li>
-        </ul>
+        <div className={'itemData'}>
+            <div className={'itemDetails'}>
+                <p className={'airline'}>A</p>
+                <ul className={'fromData'}>
+                    <li></li>
+                    <li>{segments[0].departure.iataCode}</li>
+                    <li></li>
+                </ul>
+                <ul className={'segmentsData'}>
+                    <li>{FlightDurationConverter(duration)}</li>
+                </ul>
+                <ul className={'toData'}>
+                    <li></li>
+                    <li>{segments[segments.length-1].arrival.iataCode}</li>
+                    <li></li>
+                </ul>
+            </div> 
+            <p>Operated by <span></span></p>
+        </div>
+        
     )
 }
 
 const ResultItem = ({flight, flightSearch}) =>{
     const {itineraries, price, id} = flight
     return(
-        <li>
-            {itineraries.map((f,i)=><ItemContent key={i} data={f}/>)}
-            <p>${price.grandTotal}</p>
-            <button>
-                <Link to={`/payment/${btoa(JSON.stringify(flightSearch))}/${id}`}>Book now</Link>
-            </button>
+        <li className={'itemContainer'}>
+            <div className={'itemDataContainer'}>{itineraries.map((f,i)=><ItemContent key={i} data={f}/>)}</div>
+            <div className={'booking'}>
+                <p>${price.grandTotal}</p> 
+                <p>Economy</p>
+                <button>
+                    <Link to={`/payment/${btoa(JSON.stringify(flightSearch))}/${id}`}>Book now</Link>
+                </button>
+            </div>
         </li>
     )
 }
